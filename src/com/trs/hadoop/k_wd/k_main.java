4120dc1fe4bef7beffe8acd8587c01924b2a70bc
smile870 <870730058@qq.com>
2017-04-05 13:46:57 +0800

    第二次修改

--- src/com/trs/hadoop/k_wd/k_main.java
+++ src/com/trs/hadoop/k_wd/k_main.java
@@ -19,6 +19,8 @@ import com.sun.jersey.core.impl.provider.entity.XMLJAXBElementProvider.Text;
 public class k_main {
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		
+		
 		System.out.println("Èç¹ûÐÞ¸ÄÄØ£¿");
 		Configuration conf=new Configuration();
 		Job job=new Job(conf, "hello wd");
